a7748bf84144a047d5c42aa076f8e37b8a0a078c
Mongke22 <ddd.ddd99.99@mail.ru>
2023-02-11 13:26:04 +0300

    Main activity redesign

--- app/src/main/java/com/example/astonlesson2/MainActivity.kt
+++ app/src/main/java/com/example/astonlesson2/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.astonlesson2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    override fun attachBaseContext(newBase: Context?) {
+        val context: Context = LanguageConfig.changeLanguage(newBase!!, "en")
+        super.attachBaseContext(context)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.btn_switchToSecondActivity)
+        btn.setOnClickListener{
+        }
     }
 }
