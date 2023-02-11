5ecd91f4929421b0a5705109c64162f571e09031
Mongke22 <ddd.ddd99.99@mail.ru>
2023-02-11 14:01:37 +0300

    реализация логики первого экрана

--- app/src/main/java/com/example/astonlesson2/MainActivity.kt
+++ app/src/main/java/com/example/astonlesson2/MainActivity.kt
@@ -2,21 +2,48 @@ package com.example.astonlesson2
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
+
+    private lateinit var btnSwitchToRussian: Button
+    private lateinit var btnSwitchToEnglish: Button
+    private lateinit var btnSwitchToSecondActivity: Button
+
     override fun attachBaseContext(newBase: Context?) {
-        val context: Context = LanguageConfig.changeLanguage(newBase!!, "en")
+        val context: Context = LanguageConfig.changeLanguage(newBase!!, LanguageConfig.currentLanguage)
         super.attachBaseContext(context)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btn = findViewById<Button>(R.id.btn_switchToSecondActivity)
-        btn.setOnClickListener{
+        btnSwitchToRussian = findViewById<Button>(R.id.btn_russian)
+        btnSwitchToEnglish = findViewById<Button>(R.id.btn_english)
+        btnSwitchToSecondActivity = findViewById<Button>(R.id.btn_switchToSecondActivity)
+        setListeners()
+    }
+
+    private fun setListeners(){
+        btnSwitchToEnglish.setOnClickListener{
+            LanguageConfig.currentLanguage = "en"
+            recreate()
+        }
+        btnSwitchToRussian.setOnClickListener {
+            LanguageConfig.currentLanguage = "ru"
+            recreate()
+        }
+        btnSwitchToSecondActivity.setOnClickListener {
+            val activityLauncher = registerForActivityResult(SecondActivityContract()) { result ->
+                val someText = findViewById<TextView>(R.id.tv_someText)
+                someText.text = result
+            }
+            activityLauncher.launch(Unit)
         }
     }
 }
