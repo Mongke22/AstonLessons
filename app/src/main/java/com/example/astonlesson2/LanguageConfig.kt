374b644122bd102cb700f520eee8376d5db47946
Mongke22 <ddd.ddd99.99@mail.ru>
2023-02-11 13:21:39 +0300

    language config class

--- app/src/main/java/com/example/astonlesson2/LanguageConfig.kt
+++ app/src/main/java/com/example/astonlesson2/LanguageConfig.kt
@@ -1,4 +1,24 @@
 package com.example.astonlesson2
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.res.Configuration
+import android.content.res.Resources
+
+import android.os.Build
+import java.util.*
+
+
 class LanguageConfig {
+    companion object {
+        fun changeLanguage(context: Context, languageCode: String): ContextWrapper {
+            val resources: Resources = context.resources
+            val configuration: Configuration = resources.configuration
+            val locale = Locale(languageCode)
+            Locale.setDefault(locale)
+            configuration.setLocale(locale)
+
+            return ContextWrapper(context.createConfigurationContext(configuration))
+        }
+    }
 }
