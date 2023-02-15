9ecdc06e3ba9b050310b1cad8655bee001759ff0
Mongke22 <ddd.ddd99.99@mail.ru>
2023-02-15 17:20:37 +0300

    реализация реакции на нажатие кнопок

--- app/src/main/java/com/example/astonlesson2/MainActivity.kt
+++ app/src/main/java/com/example/astonlesson2/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.astonlesson2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import androidx.constraintlayout.widget.Group
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firstBtn: Button
+    private lateinit var secondBtn: Button
+    private lateinit var thirdBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firstBtn = findViewById(R.id.firstBtn)
+        secondBtn = findViewById(R.id.secondBtn)
+        thirdBtn = findViewById(R.id.thirdBtn)
+
+        setListeners()
+    }
+
+    private fun setListeners(){
+        firstBtn.setOnClickListener {
+            hideButtonsAndShowProgress()
+        }
+        secondBtn.setOnClickListener {
+            hideButtonsAndShowProgress()
+        }
+        thirdBtn.setOnClickListener {
+            hideButtonsAndShowProgress()
+        }
+    }
+
+    private fun hideButtonsAndShowProgress(){
+        val groupToHide = findViewById<Group>(R.id.groupToHide)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        groupToHide.visibility = View.GONE
+        progressBar.visibility = View.VISIBLE
     }
 }
