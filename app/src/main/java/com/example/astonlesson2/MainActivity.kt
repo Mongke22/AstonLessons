d46b210b1eaf26479c057e0cdcd925168b636c09
Mongke22 <ddd.ddd99.99@mail.ru>
2023-02-19 17:28:00 +0300

    Демонстрация работы часов в активити

--- app/src/main/java/com/example/astonlesson2/MainActivity.kt
+++ app/src/main/java/com/example/astonlesson2/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.astonlesson2
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clock = findViewById<MyCustomClockVIew>(R.id.myCustomClock)
+        GlobalScope.launch(Dispatchers.Main) {
+            for(i in 1..100){
+                if(i % 2 == 0){
+                    clock.secondHandColor = Color.BLACK
+                    clock.secondHandWidth = 40
+                    clock.secondHandLength = 80
+                }
+                else{
+                    clock.secondHandColor = Color.GREEN
+                    clock.secondHandWidth = 10
+                    clock.secondHandLength = 20
+                }
+                delay(1000)
+                clock.updateTimer()
+            }
+        }
     }
 }
