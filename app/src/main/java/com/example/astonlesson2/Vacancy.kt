ded6586e1a262ae2e1c5fbb3a1c215b3fd66326b
Mongke22 <ddd.ddd99.99@mail.ru>
2023-02-24 17:41:28 +0300

    Класс для вакансий

---
+++ app/src/main/java/com/example/astonlesson2/Vacancy.kt
@@ -0,0 +1,11 @@
+package com.example.astonlesson2
+
+import android.net.Uri
+import java.io.Serializable
+
+data class Vacancy(
+    val id: Int,
+    var imageSource: Int,
+    var title: String,
+    var subTitle: String
+): Serializable
