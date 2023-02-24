2b1ff8af24ff6a1396437df533202f7d7363b295
Mongke22 <ddd.ddd99.99@mail.ru>
2023-02-24 17:40:18 +0300

    Реализация перехода на экран с вакансиями со списком вакансий

--- app/src/main/java/com/example/astonlesson2/Navigator.kt
+++ app/src/main/java/com/example/astonlesson2/Navigator.kt
@@ -16,7 +16,24 @@ object Navigator {
     }
 
     fun moveToVacanciesList(){
-
+        val myList: ArrayList<Vacancy> = arrayListOf(
+            Vacancy(1, R.drawable.aston_logo, "Title", "subtitle"),
+            Vacancy(2, R.drawable.aston_logo, "Title1", "subtitle"),
+            Vacancy(3, R.drawable.aston_logo, "Title2", "subtitle"),
+            Vacancy(4, R.drawable.aston_logo, "Title3", "subtitle"),
+            Vacancy(5, R.drawable.aston_logo, "Title4", "subtitle"),
+            Vacancy(6, R.drawable.aston_logo, "Title5", "subtitle"),
+            Vacancy(7, R.drawable.aston_logo, "Title6", "subtitle"),
+            Vacancy(8, R.drawable.aston_logo, "Title7", "subtitle"),
+            Vacancy(9, R.drawable.aston_logo, "Title8", "subtitle"),
+            Vacancy(10, R.drawable.aston_logo, "Title9", "subtitle"),
+            Vacancy(11, R.drawable.aston_logo, "Title10", "subtitle"),
+            Vacancy(12, R.drawable.aston_logo, "Title11", "subtitle"),
+        )
+        mainActivity!!.supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, VacanciesListFragment.newInstance(myList))
+            .addToBackStack(null)
+            .commit()
     }
 
     fun moveToOfficesList(){
